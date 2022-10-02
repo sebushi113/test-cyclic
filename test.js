8561fc400a4c84920908dc337056aecbb72cbbc1
sebushi113 <sebushi113@users.noreply.github.com>
2022-10-02 02:22:06 -0400

    make it better

--- test.js
+++ test.js
@@ -1,7 +1,32 @@
-import moment from "moment";
+// import moment from "moment";
+const { Api, JsonRpc } = require("eosjs");
+const { JsSignatureProvider } = require("eosjs/dist/eosjs-jssig.js"); // development only
+const fetch = require("node-fetch"); //node only
+const { TextDecoder, TextEncoder } = require("util"); //node only
+require("dotenv").config();
 
-const date = "YYYY-MM-DD HH:mm:ss";
-console.log(`  🦁  | ${moment(new Date()).format(date)}`);
+// const privateKeys = [privateKey1];
+
+const privateKeys = [process.env.cs1c];
+const signatureProvider = new JsSignatureProvider(privateKeys);
+const rpc = new JsonRpc("https://wax.greymass.com/", { fetch });
+const api = new Api({
+  rpc,
+  signatureProvider,
+  textDecoder: new TextDecoder(),
+  textEncoder: new TextEncoder(),
+}); //required to submit transactions
+
+(async () => {
+  const tx = await rpc.history_get_transaction(
+    "ac71f2c6e71027341036c10187be93c3ff4bfdc4de5b047ccb24fb9b966c9083",
+    205863693
+  );
+  console.log(tx);
+})();
+
+// const date = "YYYY-MM-DD HH:mm:ss";
+// console.log(`  🦁  | ${moment(new Date()).format(date)}`);
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
