3a87ddc0bf9e663b48b184c971b04174d91c5ac7
sebushi113 <41740143+sebushi113@users.noreply.github.com>
2023-06-14 15:12:54 -0400

    end test

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch"); //node only
 const { TextDecoder, TextEncoder } = require("util"); //node only
 require("dotenv").config();
 
-// testing submodule command git submodule update --init --recursive
+// testing submodule command git submodule update --init --recursive (didn't work)
 // const privateKeys = [privateKey1];
 
 const privateKeys = [process.env.cs1c];
